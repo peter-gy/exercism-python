a8c2c6cadbefd2de283956a2ae903f5f81748508
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-08-25 08:36:11 +0200

    one liner pangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,6 +1,4 @@
 from string import ascii_lowercase
 
-ALPHABET = set(ascii_lowercase)
-
 def is_pangram(sentence):
-    return ALPHABET.issubset(sentence.lower())
+    return all(character in sentence.lower() for character in ascii_lowercase)
