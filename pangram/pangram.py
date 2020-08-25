c78733578819b1f9ce3f7c035567c991343e2358
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-08-25 08:34:14 +0200

    set implementation of pangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,7 +1,6 @@
-import string
+from string import ascii_lowercase
 
-alphabet_list = list(string.ascii_lowercase)
+ALPHABET = set(ascii_lowercase)
 
 def is_pangram(sentence):
-    character_list = list(sentence.lower())
-    return all(character in character_list for character in alphabet_list)
+    return ALPHABET.issubset(sentence.lower())
