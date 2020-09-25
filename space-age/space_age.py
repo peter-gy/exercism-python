734845c6c8f8418771eeec88f65a48ede853d3e9
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-09-25 11:06:46 +0200

    space-age iteration 2

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,44 +1,20 @@
-class SpaceAge:
+class SpaceAge(object):
 
-    periodMap = {
-        "mercury": 0.2408467,
-        "venus": 0.61519726,
-        "earth": 1.0,
-        "mars": 1.8808158,
-        "jupiter": 11.862615,
-        "saturn": 29.447498,
-        "uranus": 84.016846,
-        "neptune": 164.79132,
-    }
-
-    EARTH_YEAR_IN_SECONDS = 31557600
+    PLANET_RATIOS = [(k, v * 31557600) for k, v in (
+        ('earth', 1.0),
+        ('mercury', 0.2408467),
+        ('venus', 0.61519726),
+        ('mars', 1.8808158),
+        ('jupiter', 11.862615),
+        ('saturn', 29.447498),
+        ('uranus', 84.016846),
+        ('neptune', 164.79132)
+    )]
 
     def __init__(self, seconds):
         self.seconds = seconds
+        for planet, ratio in self.PLANET_RATIOS:
+            setattr(self, 'on_' + planet, self._planet_years(ratio))
 
-    def __on(self, planet):
-        return round(self.seconds / SpaceAge.EARTH_YEAR_IN_SECONDS / SpaceAge.periodMap[planet], 2)
-
-    def on_mercury(self):
-        return self.__on('mercury')
-
-    def on_venus(self):
-        return self.__on('venus')
-
-    def on_earth(self):
-        return self.__on('earth')
-
-    def on_mars(self):
-        return self.__on('mars')
-
-    def on_jupiter(self):
-        return self.__on('jupiter')
-
-    def on_saturn(self):
-        return self.__on('saturn')
-
-    def on_uranus(self):
-        return self.__on('uranus')
-
-    def on_neptune(self):
-        return self.__on('neptune')
+    def _planet_years(self, ratio):
+        return lambda ratio=ratio: round(self.seconds / ratio, 2)
